a3c1e52a80c182809aeed6b66da5564e4a02ca13
Alberto Delbone <albertodelbone@STR-190.local>
2022-05-02 12:23:54 -0300

    Adding unit tests

--- src/my_module.py
+++ src/my_module.py
@@ -1,2 +1,3 @@
-def addOne(number):
-    return number + 1
+def get_cheaper_hotel(number):   #DO NOT change the function's name
+    cheaper_hotel = "cheaper_hotel"
+    return cheaper_hotel
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -3,6 +3,15 @@ from context import src
 from src import my_module
 
 class MyTest(TestCase):
-    def test(self):
-        result = my_module.addOne(1)
-        self.assertEqual(result, 3)
+    def test1(self):
+        result = "Lakewood"
+        self.assertEqual(result, get_cheaper_hotel("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)"))
+
+    def test1(self):
+        result = "Bridgewood"
+        self.assertEqual(result, get_cheaper_hotel("Regular: 20Mar2009(fri), 21Mar2009(sat), 22Mar2009(sun)"))
+
+    def test1(self):
+        result = "Ridgewood"
+        self.assertEqual(result, get_cheaper_hotel("Regular: 16Mar2009(mon), 17Mar2009(tues), 18Mar2009(wed)"))
+
